a64e91537a25990ca8fa6a450e1c29b6d827fea4
Jason Raubenheimer <raubenheimer.jason@gmail.com>
2022-10-01 10:18:59 +0100

    Chapter 2 done - basic functions

---
+++ OpenCVCourceMurtaza/Chapter2.cpp
@@ -0,0 +1,46 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp> // using this
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+// basic functions
+
+void main()
+{
+	string path = "Resources/test.png";
+	Mat img = imread(path);
+
+	// Grey
+	Mat imgGrey;
+	cvtColor(img, imgGrey, COLOR_BGR2GRAY);
+
+	// Blur
+	Mat imgBlur;
+	GaussianBlur(img, imgBlur, Size(3, 3), 3, 0); // kernal=3x3, 
+
+	// Edge detection
+	Mat imgCanny;
+	Canny(imgBlur, imgCanny, 50, 150);
+
+	// Dialate
+	Mat imgDia;
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3)); // creates kernal, increase size = dialate more, use odd numbers
+	dilate(imgCanny, imgDia, kernel);
+
+	// Erode
+	Mat imgErode;
+	erode(imgDia, imgErode, kernel); // doesnt have to be the same kernel
+
+	// Display images
+	imshow("Image", img);
+	imshow("Image Grey", imgGrey);
+	imshow("Image Blur", imgBlur);
+	imshow("Image Canny", imgCanny);
+	imshow("Image Dialate", imgDia);
+	imshow("Image Erode", imgErode);
+
+	waitKey(0);
+}
